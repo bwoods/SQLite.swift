ef1a56bc96cbd8b117964c3a9c5cda1bf53b87a2
Bryan Woods <woods.bryan@gmail.com>
2016-07-16 23:40:43 -0700

    Zero is a valid lastInsertRowid
    As mentioned in #409:
    
    > In #383, @stephencelis said "My take: the main insert() method shouldn't return an optional." I think that is OK, but it's at odds with the assumption inside `Connection.lastInsertRowid` that `sqlite3_last_insert_rowid() == 0` always means "not set". I think that, since rowid can be an alias to a user-defined column, it's lastInsertRowid that is wrong, and it should always return the integer that it gets, even if that is 0.

--- SQLite/Typed/Query.swift
+++ SQLite/Typed/Query.swift
@@ -965,7 +965,7 @@ extension Connection {
         let expression = query.expression
         return try sync {
             try self.run(expression.template, expression.bindings)
-            return self.lastInsertRowid!
+            return self.lastInsertRowid
         }
     }
 
